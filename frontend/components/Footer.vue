c219762157e8be0af1aaaff7ca5edf751f56de71
mariopino <mariopino@protonmail.com>
2021-03-15 20:13:18 +0100

    simple footer

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -1,102 +1,41 @@
 <template>
-  <div>
-    <div class="container">
-      <div
-        class="row text-center"
-        style="
-          margin-bottom: -48px;
-          margin-top: 70px;
-          box-shadow: rgba(65, 69, 88, 0.1) 0 0 5px 3px;
-        "
-      >
-        <div class="col-md-2 social-box">
-          <br />
-          <p>FOLLOW US</p>
-        </div>
-        <div class="col-md-2 col-6 social-box">
-          <a href="https://twitter.com/ReefDeFi" target="_blank">
-            <img src="img/icons/twitter 1.png" width="50px" alt="" />
-          </a>
-        </div>
-        <div class="col-md-2 col-6 social-box">
-          <a href="https://www.linkedin.com/company/reef-defi/" target="_blank">
-            <img src="img/icons/linkedin 1.png" width="50px" alt="" />
-          </a>
-        </div>
-        <div class="col-md-2 col-6 social-box">
-          <a href="https://www.reddit.com/r/ReefDeFi/" target="_blank">
-            <img src="img/icons/reddit 1.png" width="50px" alt="" />
-          </a>
-        </div>
-        <div class="col-md-2 col-6 social-box">
-          <a href="https://t.me/reefdefi" target="_blank">
-            <img src="img/icons/Telegram.png" width="50px" alt="" />
-          </a>
-        </div>
-        <div class="col-md-2 social-box">
-          <a href="https://medium.com/@ReefDefi" target="_blank">
-            <img src="img/icons/LogoMedium.png" width="50px" alt="" />
-          </a>
+  <div class="container-fluid footer mt-4">
+    <footer class="footer text-muted">
+      <div class="container">
+        <div class="row pt-3">
+          <div class="col-lg-2">
+            <p>
+              Copyright © {{ new Date().getFullYear() }},
+              <a href="https://reef.finance" target="_blank">REEF</a>
+            </p>
+          </div>
+          <div class="col-lg-10 text-lg-right">
+            <ul class="list-inline">
+              <li class="list-inline-item">
+                <a href="https://reef.finance" target="_blank">Website</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://github.com/reef-defi" target="_blank"
+                  >GitHub</a
+                >
+              </li>
+              <li class="list-inline-item">
+                <a href="https://www.reef.finance/privacy.html" target="_blank"
+                  >Privacy</a
+                >
+              </li>
+              <li class="list-inline-item">
+                <a
+                  href="https://www.reef.finance/terms_and_conditions.html"
+                  target="_blank"
+                  >Terms &amp; Conditions</a
+                >
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="container-fluid footer">
-      <footer>
-        <div class="row">
-          <div class="col-md-2 footer-logo">
-            <a class="navbar-brand" href="">
-              <img src="img/Reef.png" width="100px" />
-            </a>
-          </div>
-          <div class="col-md-3 text-center">
-            <ul>
-              <li class="nav-item">
-                <a href="https://reef.finance/#ecosystem">Ecosystem</a>
-              </li>
-              <li class="nav-item">
-                <a href="https://reef.finance/#liquidity">Liquidity</a>
-              </li>
-              <li class="nav-item">
-                <a href="https://reef.finance/#whyus">Why Reef</a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-md-3 text-center">
-            <ul>
-              <li class="nav-item">
-                <a href="https://reef.finance/#faq">FAQ</a>
-              </li>
-              <li class="nav-item">
-                <a href="https://reef.finance/#roadmap">Roadmap</a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-md-3 text-center">
-            <ul>
-              <li class="nav-item">
-                <a href="https://reef.finance/#media">Media</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <br />
-        <div class="row bottom-row">
-          <div class="col-md-2 offset-md-1">
-            <p>Copyright © 2021, REEF</p>
-          </div>
-          <div class="col-md-1 offset-md-6">
-            <a href="https://www.reef.finance/privacy.html">
-              <p>Privacy Policy</p>
-            </a>
-          </div>
-          <div class="col-md-2">
-            <a href="https://www.reef.finance/terms_and_conditions.html">
-              <p>Terms &amp; Conditions</p>
-            </a>
-          </div>
-        </div>
-      </footer>
-    </div>
+    </footer>
   </div>
 </template>
 <script>
