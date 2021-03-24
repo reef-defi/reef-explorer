4921f7bcdf3577aebdd4e3382f99adc7826746ff
mariopino <mariopino@protonmail.com>
2021-03-24 10:51:21 +0100

    lint

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -12,7 +12,9 @@
                 <a href="https://docs.reef.finance" target="_blank">Docs</a>
               </li>
               <li class="list-inline-item">
-                <a href="https://github.com/reef-defi" target="_blank">GitHub</a>
+                <a href="https://github.com/reef-defi" target="_blank"
+                  >GitHub</a
+                >
               </li>
             </ul>
           </div>
