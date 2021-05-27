1dfac7bfe9cac665ae9482cef537ddfa2a30d175
mariopino <mariopino@protonmail.com>
2021-05-28 00:08:44 +0200

    feat(verificator): kick-off verificator

---
+++ verificator/index.js
@@ -0,0 +1,160 @@
+const fetch = require('node-fetch');
+let solc = require("solc");
+const pino = require('pino');
+const logger = pino();
+const loggerOptions = {};
+
+// Configuration
+const nodeRpc = 'https://testnet.reefscan.com/api/v3';
+const contractName = 'Storage';
+
+const getPendingRequests = async () => {
+  const query = `
+    query contract_verification_request {
+      contract_verification_request(where: { status: { _eq: "PENDING" } }) {
+        id,
+        contract_id,
+        source,
+        filename,
+        compiler_version,
+        optimization,
+        runs,
+        target
+      }
+    }`;
+  const response = await fetch(nodeRpc, {method: 'POST', body: JSON.stringify({query})});
+  const { data } = await response.json();
+  return data.contract_verification_request;
+}
+
+const getOnChainContractBytecode = async (contract_id) => {
+  const query = `
+    query contract {
+      contract(where: { contract_id: { _eq: "${contract_id}" } }) {
+        bytecode
+      }
+    }`;
+  const response = await fetch(nodeRpc, {method: 'POST', body: JSON.stringify({query})});
+  const { data } = await response.json();
+  return data.contract[0].bytecode || '';
+}
+
+const loadCompiler = async (version) => (
+  new Promise((resolve, reject) => {
+    solc.loadRemoteVersion(version, (err, solcSnapshot) => {
+      err ? reject(err) : resolve(solcSnapshot);
+    });
+  })
+)
+
+const preprocessBytecode = async (bytecode) => {
+  const start = bytecode.lastIndexOf('6080604052');
+  const end = bytecode.lastIndexOf('a2646970667358221220');
+  return bytecode.slice(start, end)
+}
+
+const checkIfContractExists = async (bytecode, existing) => {
+  const core = await preprocessBytecode(bytecode);
+  const result = existing.find(([name, contract]) => contract === core);
+  return result !== undefined;
+}
+
+const prepareSolcContracts = (contracts) => ({
+  language: 'Solidity',
+  sources: {...contracts},
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*']
+      }
+    }
+  }
+});
+
+const prepareOptimizedSolcContracts = (contracts, runs, evmVersion) => ({
+  language: 'Solidity',
+  sources: {...contracts},
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs
+    },
+    evmVersion,
+    outputSelection: {
+      '*': {
+        '*': ['*']
+      }
+    }
+  }
+});
+
+const preprocessExistingBytecodes = async (bytecodes) => 
+  await Promise.all(
+    bytecodes.map(async ([name, code]) => [name, await preprocessBytecode(code)]));
+
+const findBytecode = async (compiler, filename, existingBytecodes, inputs) => {
+  const contracts = JSON.parse(compiler.compile(JSON.stringify(inputs)));
+  const bytecode = contracts.contracts[filename][contractName].evm.bytecode.object;
+  return await checkIfContractExists(bytecode, existingBytecodes)
+}
+
+const verify = async (request) => {
+  try {
+    const {
+      id,
+      contract_id,
+      source,
+      filename,
+      compiler_version,
+      optimization,
+      runs,
+      target
+    } = request
+    logger.info({ request: id }, `Processing contract verification request for contract ${contract_id}`);
+    const onChainContractBytecode = await getOnChainContractBytecode(contract_id);
+    const existingBytecodes = [['', onChainContractBytecode]];
+    const existing = await preprocessExistingBytecodes(existingBytecodes);
+    const compiler = await loadCompiler(compiler_version);
+    const contracts = [];
+    contracts[filename] = { content: source } 
+    
+    const normalResult = await findBytecode(
+      compiler,
+      filename,
+      existing,
+      prepareSolcContracts(contracts)
+    );
+    const optimizedResult = await findBytecode(
+      compiler,
+      filename,
+      existing,
+      prepareOptimizedSolcContracts(contracts, runs, target)
+    );
+
+    const isVerified = normalResult || optimizedResult;
+    logger.info({ request: id }, `Contract ${contract_id} is ${isVerified ? "verified" : "not verified"}`);
+
+    if (isVerified) {
+      // TODO: update request status
+
+      // TODO: insert source code, name, abi, etc in contract and set contract verified = true
+
+    } else {
+      // TODO: update request status
+    }
+
+    // TODO: delete request older than 1 week
+
+  } catch (e) {
+    logger.error({ request: id }, `Error: ${error}`);
+  }
+}
+
+const main = async () => {
+  const pendingRequests = await getPendingRequests();
+  for (request of pendingRequests) {
+    await verify(request);
+  }
+}
+
+main();
