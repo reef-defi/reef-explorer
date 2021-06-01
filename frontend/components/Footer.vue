7a85200b337f719bad22f3d9a0babbc330d62cb4
mariopino <mariopino@protonmail.com>
2021-06-01 22:07:34 +0200

    feat(frontend): move verif contract link

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -3,7 +3,14 @@
     <footer class="text-muted">
       <div class="container">
         <div class="row pt-3">
-          <div class="col-lg-12 text-lg-right">
+          <div class="col-lg-6">
+            <ul class="list-inline">
+              <li class="list-inline-item">
+                <nuxt-link to="/verifyContract">Verify Contract</nuxt-link>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-6 text-lg-right">
             <ul class="list-inline">
               <li class="list-inline-item">
                 <a href="https://reef.finance" target="_blank">Website</a>
@@ -15,9 +22,6 @@
                 <a href="https://github.com/reef-defi" target="_blank"
                   >GitHub</a
                 >
-              </li>
-              <li class="list-inline-item">
-                <nuxt-link to="/verifyContract">Verify Contract</nuxt-link>
               </li>
             </ul>
           </div>
