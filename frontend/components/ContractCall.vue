7e331c074525c69ea527c68a1a77eee27e7d98b2
mariopino <mariopino@protonmail.com>
2021-08-11 15:34:15 +0200

    feat(frontend): function arguments

--- frontend/components/ContractCall.vue
+++ frontend/components/ContractCall.vue
@@ -1,30 +1,41 @@
 <template>
   <div class="contract-call">
-    <h4 class="mt-4 mb-4">Available methods:</h4>
+    <!-- <h4 class="mt-4 mb-4">Available methods:</h4> -->
+    <!-- <pre>{{ JSON.stringify(contractInterface, null, 2) }}</pre> -->
     <div
       v-for="(message, index) in contractAbi.filter(
         (item) => item.type === 'function'
       )"
       :key="`message-${index}`"
     >
-      <p>
-        <strong>{{ message.name }}</strong> ({{ getInputs(message) }})
-        <font-awesome-icon
-          v-if="message.stateMutability !== 'view'"
-          icon="database"
+      <b-card class="mb-4">
+        <b-card-title>
+          <strong>{{ message.name }}</strong> ({{ getInputs(message) }})
+          <font-awesome-icon
+            v-if="message.stateMutability !== 'view'"
+            icon="database"
+          />
+          <span v-if="message.outputs.length > 0">
+            : {{ getOutputs(message) }}
+          </span>
+        </b-card-title>
+        <contract-call-function
+          :contract-interface="contractInterface"
+          :contract-abi="contractAbi"
+          :function-name="message.name"
         />
-        <span v-if="message.outputs.length > 0">
-          : {{ getOutputs(message) }}
-        </span>
-      </p>
+      </b-card>
     </div>
   </div>
 </template>
 
 <script>
+import { ethers } from 'ethers'
 import commonMixin from '@/mixins/commonMixin.js'
+import ContractCallFunction from './ContractCallFunction.vue'
 
 export default {
+  components: { ContractCallFunction },
   mixins: [commonMixin],
   props: {
     contractId: {
@@ -40,7 +51,17 @@ export default {
       default: () => [],
     },
   },
+  computed: {
+    contractInterface() {
+      const iface = new ethers.utils.Interface(this.contractAbi)
+      return iface.functions
+    },
+  },
   methods: {
+    getInterface(contractAbi) {
+      const iface = new ethers.utils.Interface(contractAbi)
+      return iface.functions
+    },
     getInputs(message) {
       const inputs = []
       for (const input of message.inputs) {
@@ -58,3 +79,10 @@ export default {
   },
 }
 </script>
+
+<style>
+.contract-call .card-title {
+  font-size: 1.2rem;
+  margin-bottom: 2rem;
+}
+</style>
---
+++ frontend/components/ContractCallFunction.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="contract-call-function">
+    <b-form :id="`form-${functionName}`" @submit="onSubmit" @reset="onReset">
+      <div v-for="input in functionInputs" :key="input.name">
+        <b-form-group
+          :id="`input-group-${input.name}`"
+          :label="`${input.name} (${input.type}):`"
+          :label-for="input.name"
+        >
+          <b-form-input :id="input.name" type="text" required></b-form-input>
+        </b-form-group>
+      </div>
+      <b-button type="submit" variant="primary2">SEND</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+// import { ethers } from 'ethers'
+import commonMixin from '@/mixins/commonMixin.js'
+
+export default {
+  mixins: [commonMixin],
+  props: {
+    contractInterface: {
+      type: Object,
+      default: () => {},
+    },
+    functionName: {
+      type: String,
+      default: () => '',
+    },
+    contractAbi: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      arguments: [],
+    }
+  },
+  computed: {
+    functionInputs() {
+      return this.contractAbi.find(
+        (method) => method.name === this.functionName
+      ).inputs
+    },
+  },
+  methods: {
+    getInputs(functionName) {
+      // eslint-disable-next-line no-console
+      console.log(
+        this.contractAbi.find((method) => method.name === functionName).inputs
+      )
+      return this.contractAbi.find((method) => method.name === functionName)
+        .inputs
+    },
+    onSubmit(event) {
+      event.preventDefault()
+      // 1. check function arguments
+      // 2. encode function call with arguments
+      // 3. submit extrinsic
+      alert(JSON.stringify(this.arguments))
+    },
+    onReset() {
+      // reset
+    },
+  },
+}
+</script>
