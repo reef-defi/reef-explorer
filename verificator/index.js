0c72c398e9f05e6c210b01d50d29525594a6eb7e
mariopino <mariopino@protonmail.com>
2021-08-16 14:34:09 +0200

    fix(verificator): debug

--- verificator/index.js
+++ verificator/index.js
@@ -126,7 +126,7 @@ const getContractArtifacts = async (compiler, filename, existingBytecode, inputs
     const contracts = JSON.parse(compiler.compile(JSON.stringify(inputs)));
 
     // debug
-    logger.info(loggerOptions, `compiler output: ${JSON.stringify(contracts)}`);
+    // logger.info(loggerOptions, `compiler output: ${JSON.stringify(contracts)}`);
 
     // filename excluding the extension should be equal to contract name in source code
     const contractName = filename.split('.')[0];
