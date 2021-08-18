d4e02c4309d432ddc2648e552d37b1265dcd56fb
mariopino <mariopino@protonmail.com>
2021-08-19 00:44:05 +0200

    feat(frontend): update mainnet ws

--- frontend/frontend.config.mainnet.sample.js
+++ frontend/frontend.config.mainnet.sample.js
@@ -5,7 +5,7 @@ export const network = {
   tokenDecimals: 18,
   ss58Format: 42,
   coinGeckoDenom: 'reef-finance',
-  nodeWs: 'wss://rpc-mainnet.reefscan.com/ws',
+  nodeWs: 'wss://rpc.reefscan.com/ws',
   backendWs: 'wss://reefscan.com/api/v3',
   backendHttp: 'https://reefscan.com/api/v3',
   verificatorApi: 'https://reefscan.com/api/verificator',
