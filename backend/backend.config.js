30c94485b3ce36dc7bc27efbfcbaf25ea8fbdf41
mariopino <mariopino@protonmail.com>
2021-09-06 20:19:28 +0200

    new backend

--- backend/backend.config.js
+++ backend/backend.config.js
@@ -1,3 +1,4 @@
+// @ts-check
 require('dotenv').config();
 
 module.exports = {
@@ -10,33 +11,48 @@ module.exports = {
     password: process.env.POSTGRES_PASSWORD || 'reef',
     port: process.env.POSTGRES_PORT || 5432,
   },
+  logLevel: process.env.LOG_LEVEL || 'info', // Use 'debug' to see DEBUG level messages
   crawlers: [
     {
-      enabled: !process.env.CRAWLER_BLOCK_LISTENER_DISABLE,
-      // eslint-disable-next-line global-require
-      module: require('./src/crawlers/blockListener'),
+      name: 'blockListener',
+      enabled: !process.env.BLOCK_LISTENER_DISABLE,
+      crawler: './crawlers/blockListener.js',
     },
     {
-      enabled: !process.env.CRAWLER_BLOCK_HARVESTER_DISABLE,
-      // eslint-disable-next-line global-require
-      module: require('./src/crawlers/blockHarvester'),
-      config: {
-        startDelay: 60 * 1000,
-        pollingTime:
-          parseInt(process.env.CRAWLER_BLOCK_LISTENER_POLLING_TIME_MS, 10)
-          || 60 * 60 * 1000,
-      },
+      name: 'blockHarvester',
+      enabled: !process.env.BLOCK_HARVESTER_DISABLE,
+      crawler: './crawlers/blockHarvester.js',
+      apiCustomTypes: process.env.API_CUSTOM_TYPES || '',
+      startDelay: parseInt(process.env.BLOCK_HARVESTER_START_DELAY_MS, 10) || 10 * 1000,
+      mode: process.env.BLOCK_HARVESTER_MODE || 'chunks',
+      chunkSize: parseInt(process.env.BLOCK_HARVESTER_CHUNK_SIZE, 10) || 10,
+      statsPrecision: parseInt(process.env.BLOCK_HARVESTER_STATS_PRECISION, 10) || 2,
+      pollingTime:
+        parseInt(process.env.BLOCK_LISTENER_POLLING_TIME_MS, 10)
+        || 60 * 60 * 1000,
     },
     {
-      enabled: !process.env.CRAWLER_ACTIVE_ACCOUNTS_DISABLE,
-      // eslint-disable-next-line global-require
-      module: require('./src/crawlers/activeAccounts'),
-      config: {
-        startDelay: 30 * 1000,
-        pollingTime:
-          parseInt(process.env.CRAWLER_ACTIVE_ACCOUNTS_POLLING_TIME_MS, 10)
-          || 30 * 60 * 1000,
-      },
+      name: 'ranking',
+      enabled: !process.env.RANKING_DISABLE,
+      crawler: './crawlers/ranking.js',
+      startDelay: parseInt(process.env.RANKING_START_DELAY_MS, 10) || 15 * 60 * 1000,
+      pollingTime:
+        parseInt(process.env.RANKING_POLLING_TIME_MS, 10)
+        || 5 * 60 * 1000,
+      historySize: 84,
+      erasPerDay: 4,
+      tokenDecimals: 12,
+      featuredTimespan: 60 * 60 * 24 * 7 * 2 * 1000, // 2 weeks
+    },
+    {
+      name: 'activeAccounts',
+      enabled: !process.env.ACTIVE_ACCOUNTS_DISABLE,
+      crawler: './crawlers/activeAccounts.js',
+      startDelay: parseInt(process.env.ACTIVE_ACCOUNTS_START_DELAY_MS, 10) || 60 * 1000,
+      chunkSize: parseInt(process.env.ACTIVE_ACCOUNTS_CHUNK_SIZE, 10) || 100,
+      pollingTime:
+        parseInt(process.env.ACTIVE_ACCOUNTS_POLLING_TIME_MS, 10)
+        || 6 * 60 * 60 * 1000, // 6 hours
     },
   ],
 };
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,34 @@
-const config = require('./backend.config.js');
-const Backend = require('./src/Backend.js');
+// @ts-check
+const pino = require('pino');
+const { spawn } = require('child_process');
+const { wait } = require('./lib/utils');
+const config = require('./backend.config');
 
-async function main() {
-  const backend = new Backend(config);
-  backend.runCrawlers();
-}
+const logger = pino();
 
-main().catch((error) => {
+const runCrawler = async (crawler) => {
+  const child = spawn('node', [`${crawler}`]);
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+  child.on('close', (exitCode) => {
+    logger.debug(`Crawler ${crawler} exit with code: ${exitCode}`);
+    return -1;
+  });
+};
+
+const runCrawlers = async () => {
+  logger.debug('Starting backend, waiting 10s...');
+  await wait(10000);
+
+  logger.debug('Running crawlers');
+  await Promise.all(
+    config.crawlers
+      .filter((crawler) => crawler.enabled)
+      .map(({ crawler }) => runCrawler(crawler)),
+  );
+};
+
+runCrawlers().catch((error) => {
   // eslint-disable-next-line no-console
   console.error(error);
   process.exit(-1);
