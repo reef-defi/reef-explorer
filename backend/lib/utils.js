2a918e27a0db2c09eaac743540b219cbb51f4ea4
mariopino <mariopino@protonmail.com>
2021-09-09 10:28:26 +0200

    extrinsic meta doc order

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -180,9 +180,9 @@ module.exports = {
     const { method } = extrinsic.toHuman().method;
     const args = JSON.stringify(extrinsic.args);
     const hash = extrinsic.hash.toHex();
-    const doc = extrinsic.meta.documentation
-      ? extrinsic.meta.documentation.toString().replace(/'/g, "''")
-      : extrinsic.meta.docs.toString().replace(/'/g, "''");
+    const doc = extrinsic.meta.docs
+      ? extrinsic.meta.docs.toString().replace(/'/g, "''")
+      : extrinsic.meta.documentation.toString().replace(/'/g, "''");
     const success = module.exports.getExtrinsicSuccess(index, blockEvents);
 
     // Fees
