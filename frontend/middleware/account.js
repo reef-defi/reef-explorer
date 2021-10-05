5e9abf36febac513d7897534357f720cf9a97f0b
mariopino <mariopino@protonmail.com>
2021-10-05 10:02:51 +0200

    fix evm address lookup, lint

--- frontend/middleware/account.js
+++ frontend/middleware/account.js
@@ -1,11 +1,12 @@
 import gql from 'graphql-tag'
+import { toChecksumAddress } from 'web3-utils'
 export default async function ({ app, route, store, redirect }) {
   const id = route.params.id
   if (id.match(/0x*/)) {
     const client = app.apolloProvider.defaultClient
     const query = gql`
       query account {
-        account(where: {evm_address: {_eq: "${id}"}}) {
+        account(where: {evm_address: {_eq: "${toChecksumAddress(id)}"}}) {
           account_id
         }
       }
