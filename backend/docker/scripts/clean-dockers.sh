646fb7e4cc271fe02cda39a4b40793ce606a0fe0
mariopino <mariopino@protonmail.com>
2021-10-07 10:25:19 +0200

    pending fixes

--- backend/docker/scripts/clean-dockers.sh
+++ backend/docker/scripts/clean-dockers.sh
@@ -2,17 +2,17 @@
 docker stop backend_crawler_1
 docker stop backend_postgres_1
 docker stop backend_graphql-engine_1
-docker stop backend_verificator-api_1
+docker stop backend_api_1
 docker stop backend_verificator_1
 
 docker rm backend_crawler_1
 docker rm backend_postgres_1
 docker rm backend_graphql-engine_1
-docker rm backend_verificator-api_1
+docker rm backend_api_1
 docker rm backend_verificator_1
 
 docker rmi crawler:latest
-docker rmi verificator-api:latest
+docker rmi api:latest
 docker rmi verificator:latest
 
 docker volume rm backend_db-data
