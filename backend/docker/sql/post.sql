6ae799089823ff1a395e576dde4b8d8cd528581d
mariopino <mariopino@protonmail.com>
2021-10-18 12:59:05 +0200

    fix 0x contracts in db

--- backend/docker/sql/post.sql
+++ backend/docker/sql/post.sql
@@ -1,1 +1,3 @@
-DELETE FROM contract WHERE block_height >= 1077077 AND block_height <= 1145000
+DELETE FROM contract WHERE block_height >= 1077077 AND block_height <= 1145000;
+UPDATE contract SET name = '', arguments = NULL, verified = false, source = NULL, compiler_version = NULL, optimization = NULL, runs = NULL, target = NULL, abi = NULL, license = NULL, is_erc20 = false, token_name = NULL, token_symbol = NULL, token_decimals = NULL, token_total_supply = NULL, token_validated = false, token_description = NULL, token_icon_url = NULL, token_coingecko_id = NULL, token_coinmarketcap_id = NULL WHERE bytecode = '0x'
+
