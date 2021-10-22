154d595b2bb2f222c8a075520a875351c4a1ed01
Žiga Franko <franko.ziga@gmail.com>
2021-10-22 13:16:46 +0200

    error middleware

--- contract-verificator-api/src/index.ts
+++ contract-verificator-api/src/index.ts
@@ -12,6 +12,11 @@ app.listen(port, () => {
   console.log(`Timezones by location application is running on port ${port}.`);
 });
 
+app.use('/', (err, req, res, next) => {
+  console.error(err);
+  res.status(404).send(err.message);
+})
+
 interface ContractVerificationID {
   id: string;
 }
@@ -39,84 +44,54 @@ interface AccountAddress {
 }
 
 app.post('/api/verificator/automatic-contract-verification', async (req: AppRequest<AutomaticContractVerificationReq>, res: Response) => {
-  try {
-    const bytecode = await compileSources(
-      req.body.filename, // TODO get contract name!
-      req.body.filename,
-      JSON.parse(req.body.source),
-      req.body.compilerVersion,
-      req.body.optimization,
-      req.body.runs
-    );
-    const verified = await checkIfContractIsVerified(bytecode);
-    const status = verified ? "VERIFIED" : "NOT VERIFIED"; // TODO
-    await contractVerificationInsert({...req.body, status});
-    res.send(status);
-  } catch (err) {
-    console.error(err);
-    res.status(400).send(err.message);
-  }
+  const bytecode = await compileSources(
+    req.body.filename, // TODO get contract name!
+    req.body.filename,
+    JSON.parse(req.body.source),
+    req.body.compilerVersion,
+    req.body.optimization,
+    req.body.runs
+  );
+  const verified = await checkIfContractIsVerified(bytecode);
+  const status = verified ? "VERIFIED" : "NOT VERIFIED"; // TODO
+  await contractVerificationInsert({...req.body, status});
+  res.send(status);
 });
 
 app.post('/api/verificator/manual-contract-verification', async (req: AppRequest<ManualContractVerificationReq>, res: Response) => {
-  try {
-    const isAuthenticated = await authenticationToken(req.body.token);
-    ensure(isAuthenticated, "Google Token Authentication failed!");
+  const isAuthenticated = await authenticationToken(req.body.token);
+  ensure(isAuthenticated, "Google Token Authentication failed!");
 
-    const bytecode = await compileSources(
-      req.body.filename, // TODO get contract name!
-      req.body.filename,
-      JSON.parse(req.body.source),
-      req.body.compilerVersion,
-      req.body.optimization,
-      req.body.runs
-    );
+  const bytecode = await compileSources(
+    req.body.filename, // TODO get contract name!
+    req.body.filename,
+    JSON.parse(req.body.source),
+    req.body.compilerVersion,
+    req.body.optimization,
+    req.body.runs
+  );
 
-    await contractVerificationInsert({...req.body, status: 'VERIFIED'});
-    await updateContractsVerificationStatus(bytecode);
-    res.send();
-  } catch (err) {
-    console.error(err);
-    res.status(400).send(err.message);
-  }
+  await contractVerificationInsert({...req.body, status: 'VERIFIED'});
+  await updateContractsVerificationStatus(bytecode);
+  res.send();
 })
 
 app.post('/api/verificator/status', async (req: AppRequest<ContractVerificationID>, res: Response) => {
-  try { 
-    const status = await contractVerificationStatus(req.body.id);
-    res.send(status);
-  } catch (err) {
-    console.error(err);
-    res.status(400).send(err.message);
-  }
+  const status = await contractVerificationStatus(req.body.id);
+  res.send(status);
 })
 
 app.post('/api/account/tokens', async (req: AppRequest<AccountAddress>, res: Response) => {
-  try {
-    const tokens = await findUserTokens(req.body.address);
-    res.send({tokens: [...tokens]});
-  } catch (err) {
-    console.error(err);
-    res.status(400).send(err.message);
-  }
+  const tokens = await findUserTokens(req.body.address);
+  res.send({tokens: [...tokens]});
 });
 
 app.get('/api/price/reef', async (_, res) => {
-  try {
-    const price = await getReefPrice();
-    res.send(price);    
-  } catch (err) {
-    console.error(err);
-    res.status(400).send(err.message);
-  }
+  const price = await getReefPrice();
+  res.send(price);    
 });
 
 app.get('/api/staking/rewards', async (_, res) => {
-  try {
-    const rewards = await findStakingRewards();
-    res.send({rewards: [...rewards]});
-  } catch (err) {
-    console.error(err);
-    res.status(400).send(err.message);
-  }
+  const rewards = await findStakingRewards();
+  res.send({rewards: [...rewards]});
 });
