c1708f6e3c7662c0fa75dcdf5ef3f2e9f3c5c9e2
Žiga Franko <franko.ziga@gmail.com>
2021-10-25 20:03:22 +0200

    tests

--- api/test/get_user_balance.js
+++ api/test/get_user_balance.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const ApiEndPoint = 'https://testnet.reefscan.com/api/user-balance';
+const ApiEndPoint = 'localhost:3000/api/user-balance';
 
 //
 // Endpoint: /api/user-balance
@@ -12,16 +12,20 @@ const ApiEndPoint = 'https://testnet.reefscan.com/api/user-balance';
 // userAddress: account id or EVM address
 //
 
+axios.get("http://localhost:3000/api/test")
+  .then((res) => res.data)
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err))
 
-axios
-  .post(ApiEndPoint, {
-    userAddress: "5F6ssGuH3Yz2HqZNM5sMocZF5rpr1eCMmccSz7F4MUYNuzic", // https://testnet.reefscan.com/account/5HKFJ94TydnDLLe8Tf5pve4YybksgX1CW55i849tLNGEp7cK
-  })
-  .then(res => {
-    // console.log('status:', res.status);
-    // console.log('response:', res);
-    console.log('data:', JSON.stringify(res.data, null, 2));
-  })
-  .catch(error => {
-    console.error('error:', error);
-  })
+// axios
+//   .post(ApiEndPoint, {
+//     userAddress: "5F6ssGuH3Yz2HqZNM5sMocZF5rpr1eCMmccSz7F4MUYNuzic", // https://testnet.reefscan.com/account/5HKFJ94TydnDLLe8Tf5pve4YybksgX1CW55i849tLNGEp7cK
+//   })
+//   .then(res => {
+//     // console.log('status:', res.status);
+//     // console.log('response:', res);
+//     console.log('data:', JSON.stringify(res.data, null, 2));
+//   })
+//   .catch(error => {
+//     console.error('error:', error);
+//   })
