20f105d0d1964a09e745341743aec5977f01d0b3
Žiga Franko <franko.ziga@gmail.com>
2021-10-25 12:48:53 +0200

    ensure object parameters

--- contract-verificator-api/src/utils.ts
+++ contract-verificator-api/src/utils.ts
@@ -4,3 +4,10 @@ export const ensure = (condition: boolean, message: string): void => {
     throw new Error(message);
   }
 }
+
+export const ensureObjectKeys = <Object extends {}, K extends keyof Object> (obj: Object, keys: K[]): void => {
+  keys
+    .forEach((key) => 
+      ensure(!!obj[key], `Parameter ${key} is missing`)
+    );
+}
