833be09bf2819fa43d68744d55dc152e60b87f20
Žiga Franko <franko.ziga@gmail.com>
2021-10-25 20:03:46 +0200

    fixing types

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,6 @@
-import express, {Response, Request} from 'express';
+import express, {Response} from 'express';
 import { compileContracts } from './compiler';
-import { authenticationToken, config, connect, getReefPrice } from './connector';
+import { authenticationToken, config, getReefPrice, query } from './connector';
 import { checkIfContractIsVerified, contractVerificationInsert, contractVerificationStatus, findStakingRewards, findUserTokens, updateContractsVerificationStatus } from './queries';
 import { AccountAddress, AppRequest, AutomaticContractVerificationReq, ContractVerificationID, ManualContractVerificationReq } from './types';
 import { ensure, ensureObjectKeys } from './utils';
@@ -17,17 +17,16 @@ app.use(express.json());
 
 app.post('/api/verificator/automatic-contract-verification', async (req: AppRequest<AutomaticContractVerificationReq>, res: Response) => {
   try {
-    ensureObjectKeys(req.body, ["name", "runs", "filename", "source", "compilerVersion", "optimization", "args", "address"]);
+    ensureObjectKeys(req.body, ["name", "runs", "filename", "source", "compilerVersion", "optimization", "args", "address", "license"]);
     const {abi, bytecode} = await compileContracts(
       req.body.name,
       req.body.filename,
       req.body.source,
       req.body.compilerVersion,
-      req.body.optimization,
+      req.body.optimization === "true",
       req.body.runs
     );
-    // const verified = await checkIfContractIsVerified(bytecode);
-    const verified = "VERIFIED";
+    const verified = await checkIfContractIsVerified(bytecode);
     const status = verified ? "VERIFIED" : "NOT VERIFIED"; // TODO
     // await contractVerificationInsert({...req.body, status});
     res.send(status);
@@ -42,17 +41,17 @@ app.post('/api/verificator/manual-contract-verification', async (req: AppRequest
     ensureObjectKeys(req.body, ["runs", "filename", "source", "compilerVersion", "optimization", 'token', "args", "address"]);
     const isAuthenticated = await authenticationToken(req.body.token);
     ensure(isAuthenticated, "Google Token Authentication failed!");
-  
+    const optimization = req.body.optimization === "true";
     const {bytecode} = await compileContracts(
       req.body.name,
       req.body.filename,
       req.body.source,
       req.body.compilerVersion,
-      req.body.optimization,
+      optimization,
       req.body.runs
     );
     // TODO license
-    await contractVerificationInsert({...req.body, status: 'VERIFIED', license: "MIT"});
+    await contractVerificationInsert({...req.body, status: 'VERIFIED', license: "MIT", optimization});
     await updateContractsVerificationStatus(bytecode);
     res.send("Verified");
   } catch (err) {
@@ -73,7 +72,7 @@ app.post('/api/verificator/status', async (req: AppRequest<ContractVerificationI
 
 app.post('/api/account/tokens', async (req: AppRequest<AccountAddress>, res: Response) => {
   try {
-    ensure(!!req.body.address, "Parameter id is missing");
+    ensure(!!req.body.address, "Parameter address is missing");
     const tokens = await findUserTokens(req.body.address);
     res.send({tokens: [...tokens]});
   } catch (err) {
@@ -92,9 +91,7 @@ app.get('/api/price/reef', async (_, res) => {
 
 app.get('/api/staking/rewards', async (_, res) => {
   try {
-    console.log("Hello");
     const rewards = await findStakingRewards();
-    console.log("rewards: ", rewards);
     res.send({rewards: [...rewards]});
   } catch (err) {
     res.status(400).send(err.message);
@@ -103,12 +100,11 @@ app.get('/api/staking/rewards', async (_, res) => {
 
 app.get('/api/test', async (_, res) => {
   try {
-    console.log("Connecting")
-    const db = await connect();
-    console.log(db)
+    const result = await query("SELECT name FROM contract LIMIT 1", []);
+    console.log(result);
+    res.send(result)
   } catch (err) {
-    console.error(err);
+    console.log(err);
     res.status(400).send(err.message);
   }
-});
-
+})
