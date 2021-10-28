42909676b951bcf4a894c8fad6e2c3657fbeb4cf
Samo Šlag <samo.slag@gmail.com>
2021-10-28 12:22:20 +0200

    Create Data component

---
+++ frontend/components/Details/Data.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="data">
+    <Table>
+      <slot />
+    </Table>
+  </div>
+</template>
+
+<script>
+import Table from '@/components/Table/Table'
+
+export default {
+  name: 'Data',
+  components: { Table },
+}
+</script>
+
+<style lang="scss">
+.data {
+  width: 100%;
+
+  .table-row {
+    &:nth-child(even) {
+      background: transparent;
+    }
+
+    &:nth-child(odd) {
+      background: rgba(#eaedf3, 0.5);
+    }
+
+    .table-cell {
+      &:first-child {
+        .table-cell__content {
+          font-weight: 600;
+        }
+      }
+    }
+  }
+}
+</style>
