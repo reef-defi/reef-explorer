5f004dba6256151be5a597f94c3f0674eca021aa
Samo Šlag <samo.slag@gmail.com>
2021-10-28 12:21:55 +0200

    Create Headline component

---
+++ frontend/components/Details/Headline.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="details-headline">
+    <span>
+      <slot />
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Headline',
+}
+</script>
+
+<style lang="scss">
+.details-headline {
+  display: flex;
+  justify-content: center;
+  font-size: 24px;
+  font-weight: 600;
+  text-align: center;
+
+  span {
+    display: inline-block;
+    background: linear-gradient(90deg, #a93185, #5531a9);
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  & + * {
+    margin-top: 25px;
+  }
+}
+</style>
