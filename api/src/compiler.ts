18483b91b10d52db6d6f7120b3cd15fcac0a9163
mariopino <mariopino@protonmail.com>
2021-10-29 12:16:45 +0200

    revert

--- api/src/compiler.ts
+++ api/src/compiler.ts
@@ -1,6 +1,4 @@
 import { ensure } from "./utils";
-import { License, Target } from './types';
-import { checkIfContractIsVerified, contractVerificationInsert, findContractBytecode } from './queries';
 
 const solc = require('solc');
 
@@ -18,7 +16,6 @@ interface SolcContracts {
   language: 'Solidity';
   sources: CompilerContracts;
   settings: {
-    evmVersion: Target;
     optimizer?: {
       enabled: true;
       runs: number;
@@ -38,11 +35,10 @@ const toCompilerContracts = (contracts: Contracts): CompilerContracts =>
     {} as CompilerContracts
   );
 
-const prepareSolcContracts = (contracts: Contracts, target: Target): SolcContracts => ({
+const prepareSolcContracts = (contracts: Contracts): SolcContracts => ({
   language: 'Solidity',
   sources: toCompilerContracts(contracts),
   settings: {
-    evmVersion: target,
     outputSelection: {
       '*': {
         '*': ['*']
@@ -51,11 +47,10 @@ const prepareSolcContracts = (contracts: Contracts, target: Target): SolcContrac
   }
 });
 
-const prepareOptimizedSolcContracts = (contracts: Contracts, target: Target, runs: number): SolcContracts => ({
+const prepareOptimizedSolcContracts = (contracts: Contracts, runs: number): SolcContracts => ({
   language: 'Solidity',
   sources: toCompilerContracts(contracts),
   settings: {
-    evmVersion: target,
     optimizer: {
       enabled: true,
       runs: runs
@@ -83,6 +78,9 @@ const preprocessBytecode = (bytecode: string): string => {
   const bzzr1MetadataEnd = filteredBytecode.indexOf('a265627a7a72315820');
   filteredBytecode = filteredBytecode.slice(0, bzzr1MetadataEnd);
 
+  // debug
+  // logger.info(loggerOptions, `processed bytecode: ${bytecode}`);
+
   return filteredBytecode;
 }
   
@@ -95,55 +93,24 @@ const loadCompiler = async (version: string): Promise<any> => (
 )
 
 type Bytecode = string;
-export const compileContracts = async (contractName: string, contractFilename: string, source: string, version: string, target: Target, optimizer?: boolean, runs=200): Promise<Bytecode> => {
+// interface CompilerResult {
+//   abi: string;
+//   bytecode: string;
+// }
+export const compileContracts = async (contractName: string, contractFilename: string, source: string, version: string, optimizer?: boolean, runs=200): Promise<Bytecode> => {
   const compiler = await loadCompiler(version);
   const contracts = JSON.parse(source);
   const solcData = optimizer 
-    ? prepareOptimizedSolcContracts(contracts, target, runs)
-    : prepareSolcContracts(contracts, target);
+    ? prepareOptimizedSolcContracts(contracts, runs)
+    : prepareSolcContracts(contracts);
 
   const compilerResult = JSON.parse(compiler.compile(JSON.stringify(solcData)));
-  ensure(compilerResult.contracts, "Compiler was not able to compile provided source/s");
   ensure(contractFilename in compilerResult.contracts, "Filename does not exist in compiled results");
   ensure(contractName in compilerResult.contracts[contractFilename], "Name does not exist in compiled results");
   const result = compilerResult.contracts[contractFilename][contractName];
-  const preprocessedBytecode = preprocessBytecode(result.evm.deployedBytecode.object);
-  return preprocessedBytecode;
-}
-
-
-export const verifyContracts = async (address: string, contractName: string, filename: string, constructorArguments: string, source: string, compilerVersion: string, target: Target, optimization: boolean, runs: number, license: License): Promise<string> => {
-  const verified = await checkIfContractIsVerified(address);
-  ensure(!verified, "Contract was already verified");
-  const bytecode = await compileContracts(
-    contractName,
-    filename,
-    source,
-    compilerVersion,
-    target,
-    optimization,
-    runs
-  );
-  ensure(bytecode.length > 0, "Compiler produced wrong output. Please contact reef team!", 404);
-  const deployedBytecode = await findContractBytecode(address);
-  const preprocessedDeployedBytecode = preprocessBytecode(deployedBytecode);
-  console.log('db:', preprocessedDeployedBytecode);
-  console.log('request:', bytecode);
-  const status =
-    preprocessedDeployedBytecode === bytecode
-      ? "VERIFIED"
-      : "NOT VERIFIED";
-  await contractVerificationInsert({
-    runs,
-    source,
-    status,
-    target,
-    address,
-    filename,
-    license,
-    constructorArguments,
-    optimization,
-    compilerVersion
-  });
-  return status;
+  return preprocessBytecode(result.evm.bytecode.object);
+  // return {
+  //   abi: JSON.stringify(result.abi),
+  //   bytecode: preprocessBytecode(result.evm.bytecode.object)
+  // }
 }
