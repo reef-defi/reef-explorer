3197c2b422edec98cbd59f2225868e7070191c02
Samo Šlag <samo.slag@gmail.com>
2021-11-02 17:05:12 +0100

    Recreate Account Token Balances

--- frontend/components/AccountTokenBalances.vue
+++ frontend/components/AccountTokenBalances.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="account-token-balances">
+  <div class="account-token-balances list-view">
     <div v-if="loading" class="text-center py-4">
       <Loading />
     </div>
@@ -7,84 +7,56 @@
       <h5>{{ $t('components.account_token_balances.no_token_found') }}</h5>
     </div>
     <div v-else>
-      <!-- Filter -->
-      <b-row style="margin-bottom: 1rem">
-        <b-col cols="12">
-          <b-form-input
-            id="filterInput"
-            v-model="filter"
-            type="search"
-            :placeholder="$t('components.account_token_balances.search')"
-          />
-        </b-col>
-      </b-row>
-      <JsonCSV
-        :data="balances"
-        class="download-csv mb-2"
-        :name="`reef_token_balances_${accountId}.csv`"
-      >
-        <font-awesome-icon icon="file-csv" />
-        {{ $t('pages.accounts.download_csv') }}
-      </JsonCSV>
-      <div class="table-responsive">
-        <b-table
-          striped
-          hover
-          :fields="fields"
-          :per-page="perPage"
-          :current-page="currentPage"
-          :items="balances"
-          :filter="filter"
-          @filtered="onFiltered"
+      <div class="list-view__table-head">
+        <Input
+          v-model="filter"
+          :placeholder="$t('components.account_token_balances.search')"
+        />
+        <JsonCSV
+          :data="balances"
+          class="list-view__download-btn"
+          :name="`reef_token_balances_${accountId}.csv`"
         >
-          <template #cell(contract_id)="data">
-            <p class="mb-0">
-              <eth-identicon :address="data.item.contract_id" :size="16" />
-              <nuxt-link
-                v-b-tooltip.hover
-                :to="`/contract/${data.item.contract_id}`"
-                title="Check contract information"
-              >
-                {{ data.item.contract_id }}
-              </nuxt-link>
-            </p>
-          </template>
-          <template #cell(token_name)="data">
-            <p class="mb-0">
-              <nuxt-link :to="`/token/${data.item.contract_id}`">
-                {{ data.item.token_name }}
-              </nuxt-link>
-            </p>
-          </template>
-          <template #cell(balance)="data">
-            <p class="mb-0">
-              {{
-                formatTokenAmount(
-                  data.item.balance,
-                  data.item.token_decimals,
-                  data.item.token_symbol
-                )
-              }}
-            </p>
-          </template>
-        </b-table>
-        <div class="mt-4 d-flex">
-          <b-pagination
-            v-model="currentPage"
-            :total-rows="totalRows"
-            :per-page="perPage"
-            aria-controls="account-token-balances-table"
-          />
-          <b-button-group class="ml-2">
-            <b-button
-              v-for="(item, index) in tableOptions"
-              :key="index"
-              @click="handleNumFields(item)"
-            >
-              {{ item }}
-            </b-button>
-          </b-button-group>
-        </div>
+          <font-awesome-icon icon="file-csv" />
+          <span>{{ $t('pages.accounts.download_csv') }}</span>
+        </JsonCSV>
+      </div>
+
+      <Table>
+        <THead>
+          <Cell>Token</Cell>
+          <Cell>Address</Cell>
+          <Cell align="right">Balance</Cell>
+        </THead>
+
+        <Row v-for="(item, index) in paginated" :key="index">
+          <Cell :link="`/token/${item.contract_id}`">{{
+            item.token_name
+          }}</Cell>
+
+          <Cell :link="{ url: `/contract/${item.contract_id}`, fill: false }">
+            <eth-identicon :address="item.contract_id" :size="20" />
+            <span>{{ item.contract_id }}</span>
+          </Cell>
+
+          <Cell align="right">
+            {{
+              formatTokenAmount(
+                item.balance,
+                item.token_decimals,
+                item.token_symbol
+              )
+            }}
+          </Cell>
+        </Row>
+      </Table>
+
+      <div class="list-view__pagination">
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="searchResults.length"
+          :per-page="perPage"
+        />
       </div>
     </div>
   </div>
@@ -96,11 +68,13 @@ import JsonCSV from 'vue-json-csv'
 import commonMixin from '@/mixins/commonMixin.js'
 import Loading from '@/components/Loading.vue'
 import { paginationOptions } from '@/frontend.config.js'
+import Input from '@/components/Input'
 
 export default {
   components: {
     JsonCSV,
     Loading,
+    Input,
   },
   mixins: [commonMixin],
   props: {
@@ -116,39 +90,39 @@ export default {
       filter: null,
       filterOn: [],
       tableOptions: paginationOptions,
-      perPage: localStorage.paginationOptions
-        ? parseInt(localStorage.paginationOptions)
-        : 10,
+      perPage: 20,
       currentPage: 1,
       totalRows: 1,
-      fields: [
-        {
-          key: 'token_name',
-          label: 'Token',
-          sortable: true,
-        },
-        {
-          key: 'contract_id',
-          label: 'Address',
-          sortable: false,
-        },
-        {
-          key: 'balance',
-          label: 'Balance',
-          sortable: true,
-        },
-      ],
     }
+  },
+  computed: {
+    searchResults() {
+      const list = this.balances || []
+
+      if (!this.filter) return list
+
+      return list.filter((item) => {
+        const filter = this.filter.toLowerCase()
+        const name = item.token_name?.toLowerCase() || ''
+        const address = item.contract_id?.toLowerCase() || ''
+
+        return name.includes(filter) || address.includes(filter)
+      })
+    },
+    paginated() {
+      const paginate = (list) => {
+        const start = this.perPage * (this.currentPage - 1)
+        const end = start + this.perPage
+        return list.slice(start, end)
+      }
+
+      return paginate(this.searchResults)
+    },
   },
   methods: {
     handleNumFields(num) {
       localStorage.paginationOptions = num
       this.perPage = parseInt(num)
-    },
-    onFiltered(filteredItems) {
-      // Trigger pagination to update the number of buttons/pages due to filtering
-      this.totalRows = filteredItems.length
-      this.currentPage = 1
     },
   },
   apollo: {
