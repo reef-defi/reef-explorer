18a2e60384eadafb64ab0e8e3253ca81f932fc9d
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2021-11-04 14:32:06 +0100

    Flipper, Greete, Storage and ERC20Contract contracts with their sources, abi and deployed bytecode

---
+++ api/test/contracts/Greete/abi.ts
@@ -0,0 +1,40 @@
+import {ABI} from "../../../src/types";
+export default [
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_greeting",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "greet",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_greeting",
+        "type": "string"
+      }
+    ],
+    "name": "setGreeting",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+] as ABI;
