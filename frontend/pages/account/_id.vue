6ae435a1da47a275cefe5681af5b2e3680d3d3a1
Samo Šlag <samo.slag@gmail.com>
2021-11-04 19:00:51 +0100

    Restyle Account details

--- frontend/pages/account/_id.vue
+++ frontend/pages/account/_id.vue
@@ -8,17 +8,63 @@
         <template v-else-if="!parsedAccount">
           <h1 class="text-center">Account not valid!</h1>
         </template>
-        <template v-else>
-          <div class="card mb-4">
-            <div class="card-body">
-              <p class="text-center mb-2">
+        <Card v-else class="account-details">
+          <div class="account-details__identicon">
+            <ReefIdenticon
+              :key="parsedAccount.accountId"
+              :address="parsedAccount.accountId"
+              :size="80"
+            />
+          </div>
+
+          <Headline
+            v-if="
+              parsedAccount.identity.display &&
+              parsedAccount.identity.displayParent
+            "
+          >
+            {{ parsedAccount.identity.displayParent }} /
+            {{ parsedAccount.identity.display }}
+          </Headline>
+          <Headline v-else-if="parsedAccount.identity.display">
+            {{ parsedAccount.identity.display }}
+          </Headline>
+          <Headline v-else>
+            {{ shortAddress(parsedAccount.accountId) }}
+          </Headline>
+
+          <Data>
+            <Row>
+              <Cell>{{ $t('details.account.account_id') }}</Cell>
+              <Cell>
                 <ReefIdenticon
                   :key="parsedAccount.accountId"
+                  class="account-details__account-identicon"
                   :address="parsedAccount.accountId"
-                  :size="80"
+                  :size="20"
                 />
-              </p>
-              <h4 class="text-center mb-4">
+                <span>{{ parsedAccount.accountId }}</span>
+              </Cell>
+            </Row>
+
+            <Row v-if="parsedAccount.evmAddress">
+              <Cell>{{ $t('details.account.evm_address') }}</Cell>
+              <Cell>
+                <eth-identicon :address="parsedAccount.evmAddress" :size="20" />
+                <span>{{ parsedAccount.evmAddress }}</span>
+              </Cell>
+            </Row>
+
+            <Row v-if="parsedAccount.evmNonce">
+              <Cell>{{ $t('details.account.evm_nonce') }}</Cell>
+              <Cell>
+                <span>{{ parsedAccount.evmNonce }}</span>
+              </Cell>
+            </Row>
+
+            <Row v-if="parsedAccount.identity.display">
+              <Cell>Identity::display</Cell>
+              <Cell>
                 <span
                   v-if="
                     parsedAccount.identity.display &&
@@ -28,213 +74,152 @@
                   {{ parsedAccount.identity.displayParent }} /
                   {{ parsedAccount.identity.display }}
                 </span>
-                <span v-else-if="parsedAccount.identity.display">
+                <span v-else>
                   {{ parsedAccount.identity.display }}
                 </span>
-                <span v-else>
-                  {{ shortAddress(parsedAccount.accountId) }}
+              </Cell>
+            </Row>
+
+            <Row v-if="parsedAccount.identity.email">
+              <Cell>Identity::email</Cell>
+              <Cell>
+                <a
+                  :href="`mailto:${parsedAccount.identity.email}`"
+                  target="_blank"
+                  >{{ parsedAccount.identity.email }}</a
+                >
+              </Cell>
+            </Row>
+
+            <Row v-if="parsedAccount.identity.legal">
+              <Cell>Identity::legal</Cell>
+              <Cell>
+                {{ parsedAccount.identity.legal }}
+              </Cell>
+            </Row>
+
+            <Row v-if="parsedAccount.identity.riot">
+              <Cell>Identity::riot</Cell>
+              <Cell>
+                {{ parsedAccount.identity.riot }}
+              </Cell>
+            </Row>
+
+            <Row v-if="parsedAccount.identity.web">
+              <Cell>Identity::web</Cell>
+              <Cell>
+                <a :href="parsedAccount.identity.web" target="_blank">{{
+                  parsedAccount.identity.web
+                }}</a>
+              </Cell>
+            </Row>
+
+            <Row v-if="parsedAccount.identity.twitter">
+              <Cell>Identity::twitter</Cell>
+              <Cell>
+                <a
+                  :href="`https://twitter.com/${parsedAccount.identity.twitter.substr(
+                    1,
+                    parsedAccount.identity.twitter.length
+                  )}`"
+                  target="_blank"
+                  >{{ parsedAccount.identity.twitter }}</a
+                >
+              </Cell>
+            </Row>
+
+            <Row v-if="parsedAccount.identity.judgements">
+              <Cell>Identity::judgements</Cell>
+              <Cell>
+                <span v-if="parsedAccount.identity.judgements.length > 0">
+                  {{ parsedAccount.identity.judgements }}
                 </span>
-              </h4>
-              <h4 class="text-center mb-4 amount">
+                <span> No </span>
+              </Cell>
+            </Row>
+
+            <Row>
+              <Cell>{{ $t('details.account.account_nonce') }}</Cell>
+              <Cell>
+                {{ parsedAccount.nonce }}
+              </Cell>
+            </Row>
+
+            <Row>
+              <Cell>{{ $t('details.account.total_balance') }}</Cell>
+              <Cell class="amount">
                 {{ formatAmount(parsedAccount.balances.freeBalance) }}
-              </h4>
-              <div class="table-responsive pb-4">
-                <table class="table table-striped">
-                  <tbody>
-                    <tr>
-                      <td>{{ $t('details.account.account_id') }}</td>
-                      <td>
-                        <ReefIdenticon
-                          :key="parsedAccount.accountId"
-                          :address="parsedAccount.accountId"
-                          :size="20"
-                        />
-                        <span>{{ parsedAccount.accountId }}</span>
-                      </td>
-                    </tr>
-                    <tr v-if="parsedAccount.evmAddress">
-                      <td>{{ $t('details.account.evm_address') }}</td>
-                      <td>
-                        <eth-identicon
-                          :address="parsedAccount.evmAddress"
-                          :size="20"
-                        />
-                        <span>{{ parsedAccount.evmAddress }}</span>
-                      </td>
-                    </tr>
-                    <tr v-if="parsedAccount.evmNonce">
-                      <td>{{ $t('details.account.evm_nonce') }}</td>
-                      <td>
-                        <span>{{ parsedAccount.evmNonce }}</span>
-                      </td>
-                    </tr>
-                    <tr v-if="parsedAccount.identity.display">
-                      <td>Identity::display</td>
-                      <td>
-                        <span
-                          v-if="
-                            parsedAccount.identity.display &&
-                            parsedAccount.identity.displayParent
-                          "
-                        >
-                          {{ parsedAccount.identity.displayParent }} /
-                          {{ parsedAccount.identity.display }}
-                        </span>
-                        <span v-else>
-                          {{ parsedAccount.identity.display }}
-                        </span>
-                      </td>
-                    </tr>
-                    <tr v-if="parsedAccount.identity.email">
-                      <td>Identity::email</td>
-                      <td>
-                        <a
-                          :href="`mailto:${parsedAccount.identity.email}`"
-                          target="_blank"
-                          >{{ parsedAccount.identity.email }}</a
-                        >
-                      </td>
-                    </tr>
-                    <tr v-if="parsedAccount.identity.legal">
-                      <td>Identity::legal</td>
-                      <td>
-                        {{ parsedAccount.identity.legal }}
-                      </td>
-                    </tr>
-                    <tr v-if="parsedAccount.identity.riot">
-                      <td>Identity::riot</td>
-                      <td>
-                        {{ parsedAccount.identity.riot }}
-                      </td>
-                    </tr>
-                    <tr v-if="parsedAccount.identity.web">
-                      <td>Identity::web</td>
-                      <td>
-                        <a :href="parsedAccount.identity.web" target="_blank">{{
-                          parsedAccount.identity.web
-                        }}</a>
-                      </td>
-                    </tr>
-                    <tr v-if="parsedAccount.identity.twitter">
-                      <td>Identity::twitter</td>
-                      <td>
-                        <a
-                          :href="`https://twitter.com/${parsedAccount.identity.twitter.substr(
-                            1,
-                            parsedAccount.identity.twitter.length
-                          )}`"
-                          target="_blank"
-                          >{{ parsedAccount.identity.twitter }}</a
-                        >
-                      </td>
-                    </tr>
-                    <tr v-if="parsedAccount.identity.judgements">
-                      <td>Identity::judgements</td>
-                      <td>
-                        <span
-                          v-if="parsedAccount.identity.judgements.length > 0"
-                        >
-                          {{ parsedAccount.identity.judgements }}
-                        </span>
-                        <span> No </span>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>{{ $t('details.account.account_nonce') }}</td>
-                      <td>
-                        {{ parsedAccount.nonce }}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>{{ $t('details.account.total_balance') }}</td>
-                      <td class="amount">
-                        {{ formatAmount(parsedAccount.balances.freeBalance) }}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>{{ $t('details.account.available_balance') }}</td>
-                      <td class="amount">
-                        {{
-                          formatAmount(parsedAccount.balances.availableBalance)
-                        }}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>{{ $t('details.account.locked_balance') }}</td>
-                      <td class="amount">
-                        {{ formatAmount(parsedAccount.balances.lockedBalance) }}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>{{ $t('details.account.reserved_balance') }}</td>
-                      <td class="amount">
-                        {{
-                          formatAmount(parsedAccount.balances.reservedBalance)
-                        }}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>{{ $t('details.account.is_vesting') }}</td>
-                      <td>
-                        {{ parsedAccount.balances.isVesting ? `Yes` : `No` }}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>{{ $t('details.account.vested_balance') }}</td>
-                      <td class="amount">
-                        {{ formatAmount(parsedAccount.balances.vestedBalance) }}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>{{ $t('details.account.vesting_total') }}</td>
-                      <td class="amount">
-                        {{ formatAmount(parsedAccount.balances.vestingTotal) }}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>{{ $t('details.account.voting_balance') }}</td>
-                      <td class="amount">
-                        {{ formatAmount(parsedAccount.balances.votingBalance) }}
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <b-tabs class="mt-4" content-class="mt-4" fill>
-                <b-tab active>
-                  <template #title>
-                    <h5>Transfers</h5>
-                  </template>
-                  <AccountTransfers :account-id="accountId" />
-                </b-tab>
-                <b-tab>
-                  <template #title>
-                    <h5>Tokens</h5>
-                  </template>
-                  <account-token-balances :account-id="accountId" />
-                </b-tab>
-                <b-tab>
-                  <template #title>
-                    <h5>Activity</h5>
-                  </template>
-                  <Activity :account-id="accountId" />
-                </b-tab>
-                <b-tab>
-                  <template #title>
-                    <h5>Rewards</h5>
-                  </template>
-                  <staking-rewards :account-id="accountId" />
-                </b-tab>
-                <b-tab>
-                  <template #title>
-                    <h5>Slashes</h5>
-                  </template>
-                  <staking-slashes :account-id="accountId" />
-                </b-tab>
-              </b-tabs>
-            </div>
-          </div>
-        </template>
+              </Cell>
+            </Row>
+
+            <Row>
+              <Cell>{{ $t('details.account.available_balance') }}</Cell>
+              <Cell class="amount">
+                {{ formatAmount(parsedAccount.balances.availableBalance) }}
+              </Cell>
+            </Row>
+
+            <Row>
+              <Cell>{{ $t('details.account.locked_balance') }}</Cell>
+              <Cell class="amount">
+                {{ formatAmount(parsedAccount.balances.lockedBalance) }}
+              </Cell>
+            </Row>
+
+            <Row>
+              <Cell>{{ $t('details.account.reserved_balance') }}</Cell>
+              <Cell class="amount">
+                {{ formatAmount(parsedAccount.balances.reservedBalance) }}
+              </Cell>
+            </Row>
+
+            <Row>
+              <Cell>{{ $t('details.account.is_vesting') }}</Cell>
+              <Cell>
+                {{ parsedAccount.balances.isVesting ? `Yes` : `No` }}
+              </Cell>
+            </Row>
+
+            <Row>
+              <Cell>{{ $t('details.account.vested_balance') }}</Cell>
+              <Cell class="amount">
+                {{ formatAmount(parsedAccount.balances.vestedBalance) }}
+              </Cell>
+            </Row>
+
+            <Row>
+              <Cell>{{ $t('details.account.vesting_total') }}</Cell>
+              <Cell class="amount">
+                {{ formatAmount(parsedAccount.balances.vestingTotal) }}
+              </Cell>
+            </Row>
+
+            <Row>
+              <Cell>{{ $t('details.account.voting_balance') }}</Cell>
+              <Cell class="amount">
+                {{ formatAmount(parsedAccount.balances.votingBalance) }}
+              </Cell>
+            </Row>
+          </Data>
+
+          <Tabs v-model="tab" :options="$options.tabs" />
+
+          <AccountTransfers
+            v-if="tab === 'transfers'"
+            :account-id="accountId"
+          />
+
+          <AccountTokenBalances
+            v-if="tab === 'tokens'"
+            :account-id="accountId"
+          />
+
+          <Activity v-if="tab === 'activity'" :account-id="accountId" />
+
+          <StakingRewards v-if="tab === 'rewards'" :account-id="accountId" />
+
+          <StakingSlashes v-if="tab === 'slashes'" :account-id="accountId" />
+        </Card>
       </b-container>
     </section>
   </div>
@@ -263,6 +248,13 @@ export default {
   },
   mixins: [commonMixin],
   middleware: ['account'],
+  tabs: {
+    transfers: 'Transfers',
+    tokens: 'Tokens',
+    activity: 'Activity',
+    rewards: 'Rewards',
+    slashes: 'Slashes',
+  },
   data() {
     return {
       network,
@@ -270,29 +262,7 @@ export default {
       accountId: this.$route.params.id,
       parsedAccount: undefined,
       transfers: [],
-      fields: [
-        {
-          key: 'block_number',
-          label: 'Block number',
-          class: 'd-none d-sm-none d-md-none d-lg-block d-xl-block',
-          sortable: true,
-        },
-        {
-          key: 'from',
-          label: 'From',
-          sortable: true,
-        },
-        {
-          key: 'to',
-          label: 'To',
-          sortable: true,
-        },
-        {
-          key: 'amount',
-          label: 'Amount',
-          sortable: true,
-        },
-      ],
+      tab: 'transfers',
     }
   },
   watch: {
@@ -382,3 +352,38 @@ export default {
   },
 }
 </script>
+
+<style lang="scss">
+.account-details {
+  .account-details__identicon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: white;
+    box-shadow: 0 0 10px -10px rgba(#0f233f, 0.5),
+      0 5px 15px -5px rgba(#0f233f, 0.25);
+    border-radius: 50%;
+    height: 110px;
+    width: 110px;
+    margin: 0 auto 15px auto;
+    overflow: hidden;
+  }
+
+  .account-details__account-identicon {
+    display: flex !important;
+  }
+
+  .amount {
+    .table-cell__content {
+      font-weight: 600;
+      background: linear-gradient(90deg, #a93185, #5531a9);
+      background-clip: text;
+      -webkit-text-fill-color: transparent;
+    }
+  }
+
+  .tabs {
+    margin: 25px 0;
+  }
+}
+</style>
