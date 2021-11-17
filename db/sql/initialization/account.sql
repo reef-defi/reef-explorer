151e363969aaea82a4ba5a6b95a33f6b0c9a417a
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2021-11-17 13:19:57 +0100

    Initial db setup

---
+++ db/sql/initialization/account.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS account (
+  evm_address VARCHAR(42) PRIMARY KEY (evm_address),
+  address VARCHAR(48) NOT NULL, -- TODO maybe we can add also address as Primary Key
+  -- TODO add additional attributes
+);
+
+-- Inserting default account
+INSERT INTO account (evm_address, address)
+VALUES
+  ('0x', '0x');
