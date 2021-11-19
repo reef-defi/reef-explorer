99b0bc2b8f9a9af80b83860fb9436653f3434206
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2021-11-19 17:57:15 +0100

    evm events initialization fixed

--- db/sql/initialization/evmEvent.sql
+++ db/sql/initialization/evmEvent.sql
@@ -1,6 +1,7 @@
 CREATE TYPE EventStatus AS ENUM ('success', 'error');
 
 CREATE TABLE IF NOT EXISTS unverified_evm_call (
+  id BIGSERIAL,
   extrinsic_id BIGINT,
   signer_address VARCHAR(48),
 
@@ -8,6 +9,7 @@ CREATE TABLE IF NOT EXISTS unverified_evm_call (
   gas_limit BIGINT NOT NULL,
   storage_limit BIGINT NOT NULL,
 
+  PRIMARY KEY (id),
   CONSTRAINT fk_extrinsic
     FOREIGN KEY (extrinsic_id)
       REFERENCES extrinsic(id),
@@ -17,6 +19,7 @@ CREATE TABLE IF NOT EXISTS unverified_evm_call (
 );
 
 CREATE TABLE IF NOT EXISTS verified_evm_event (
+  id BIGSERIAL,
   extrinsic_id BIGINT,
   signer_address VARCHAR(48),
   verified_contract_id BIGINT,
@@ -31,12 +34,13 @@ CREATE TABLE IF NOT EXISTS verified_evm_event (
   storage_limit BIGINT,
   fee BIGINT,
 
+  PRIMARY KEY (id),
   CONSTRAINT fk_extrinsic
     FOREIGN KEY (extrinsic_id)
       REFERENCES extrinsic(id),
   CONSTRAINT fk_account
     FOREIGN KEY (signer_address)
-      REFERENCES account(evm_address),
+      REFERENCES account(address),
   CONSTRAINT fk_verified_contract
     FOREIGN KEY (verified_contract_id)
       REFERENCES verified_contract(id)
