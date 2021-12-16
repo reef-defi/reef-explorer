0e0a2e95267d94ee0e5f0d4ea3da09b07c145960
Ziga Franko G <frenkie@Zigas-MBP-2.lan>
2021-12-16 15:22:24 +0100

    sentry initialization

--- crawler/src/index.ts
+++ crawler/src/index.ts
@@ -1,8 +1,23 @@
 import { APP_CONFIG } from "./config";
 import { processBlocks } from "./crawler/block";
 import { deleteUnfinishedBlocks, lastBlockInDatabase } from "./queries/block";
-import { closeProviders, initializeProviders, lastBlockId, restartNodeProviders } from "./utils/connector";
+import { closeProviders, initializeProviders, lastBlockId } from "./utils/connector";
 import { min, wait } from "./utils/utils";
+import * as Sentry from "@sentry/node";
+import { RewriteFrames } from "@sentry/integrations";
+// Importing @sentry/tracing patches the global hub for tracing to work.
+// import * as Tracing from "@sentry/tracing";
+
+Sentry.init({
+  dsn: 'https://b297f1e10e8040f693bbc66c9696f5d9@sentry.ilol.si/6', //TODO put this out! process.env.SENTRY_DSN
+  tracesSampleRate: 1.0,
+  integrations: [
+    new RewriteFrames({
+      root: global.__dirname,
+    }),
+  ],
+});
+
 
 let BLOCKS_PER_STEP = APP_CONFIG.startBlockSize;
 let currentBlockIndex = -1;
@@ -24,7 +39,7 @@ const processNextBlock = async () => {
     const bps = difference/time;
 
     console.log(`n nodes: ${APP_CONFIG.nodeUrls.length}\tn blocks: ${difference}\tbps: ${bps.toFixed(3)}\tn transactions: ${per.transactions}\ttps: ${(per.transactions/time).toFixed(3)}\ttime: ${time.toFixed(3)} s\tblock from ${from} to ${to}`)
-    console.log(`\t\tPerformance\tNode: ${(per.nodeTime / ms * 100).toFixed(2)}%\tDB: ${(per.dbTime/ms*100).toFixed(2)}%\tSys: ${(per.processingTime/ms*100).toFixed(2)}%`)
+    // console.log(`\t\tPerformance\tNode: ${(per.nodeTime / ms * 100).toFixed(2)}%\tDB: ${(per.dbTime/ms*100).toFixed(2)}%\tSys: ${(per.processingTime/ms*100).toFixed(2)}%`)
     BLOCKS_PER_STEP = min(BLOCKS_PER_STEP * 2, APP_CONFIG.maxBlocksPerStep);    
   };
 
@@ -42,6 +57,7 @@ Promise.resolve()
   .then(processNextBlock)
   .catch((error) => {
     console.error(error);
+    Sentry.captureException(error)
     process.exit(-1);
   })
   .finally(closeProviders)
