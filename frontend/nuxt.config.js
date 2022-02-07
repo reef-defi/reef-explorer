9e2c85f0831d8bb5597916425cca552d39c004fa
Matjaz Hirsman <matjazonline@gmail.com>
2022-02-07 18:32:02 +0100

    rename Reef finance

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -8,7 +8,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Explorer | Reef Finance',
+    title: 'Explorer | Reef chain',
     htmlAttrs: {
       lang: 'en',
     },
