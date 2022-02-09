ea28dc32da56210998035ad9cb9195be3816844f
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-02-09 14:49:25 +0100

    verified contract trigger

--- db/sql/initialization/contract.sql
+++ db/sql/initialization/contract.sql
@@ -61,9 +61,25 @@ CREATE TABLE IF NOT EXISTS newly_verified_contract_queue (
   address VARCHAR(48),
   CONSTRAINT fk_verified_contract
     FOREIGN KEY (address)
-      REFERENCES verified_contract(address)
+      REFERENCES contract(address)
       ON DELETE NO ACTION
 );
+
+-- When new contract is added in verified contract table we capture its address and add it in newly verified contract queueu 
+START TRANSACTION;
+CREATE FUNCTION newly_added_contract_trigger() RETURNS trigger LANGUAGE plpgsql AS
+$$BEGIN
+  IF TG_OP = 'INSERT' THEN
+    INSERT INTO newly_verified_contract_queue (address) VALUES (TG_OP.address)
+    RETURN NEW;
+END;$$;
+CREATE CONSTRAINT TRIGGER extrinsic_count_mod
+  AFTER INSERT ON verified_contract
+  DEFERRABLE INITIALLY DEFERRED
+  FOR EACH ROW EXECUTE PROCEDURE newly_added_contract_trigger();
+COMMIT;
+
+
 
 CREATE TABLE IF NOT EXISTS verification_request (
   id BIGSERIAL,
