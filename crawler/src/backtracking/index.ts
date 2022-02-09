6ad8550ecd24417cba2ac4f025825fdca70fc084
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-02-09 12:37:06 +0100

    injected backtracking logic WIP

--- crawler/src/backtracking/index.ts
+++ crawler/src/backtracking/index.ts
@@ -1,3 +1,50 @@
-export default async (events: string) => {
+import { EVMEventData, EvmLogWithDecodedEvent } from "../crawler/types";
+import { getContractDB } from "../queries/evmEvent";
+import { queryv2 } from "../utils/connector"
+import { utils } from "ethers";
+import { processTokenTransfers } from "../crawler/transfer";
+import { processEvmTokenHolders } from "../crawler/tokenHolder";
+import { insertTransfers } from "../queries/extrinsic";
+import insertTokenHolders from "../queries/tokenHoldes";
 
-};
+export default async (contractAddress: string) => {
+  // TODO update query with appropriet types
+  // TODO inject query interface
+  const evmEvents = await queryv2<EVMEventData>('SELECT * FROM evm_event WHERE address = $1 AND type = \'Unverified\'', [contractAddress]);
+  const contract = await getContractDB(contractAddress);
+
+  if (contract.length) {
+    throw new Error(`Contract address: ${contractAddress} was not found in verified contract...`);
+  }
+
+  const {compiled_data: compiledData, name, address, type, contract_data: contractData} = contract[0];
+  const contractInterface = new utils.Interface(compiledData[name]);
+
+  const evmLogs: EvmLogWithDecodedEvent[] = evmEvents
+    .filter(({method}) => method === 'Log')
+    .map(({rawData, timestamp, blockId, extrinsicId}) => {
+      const decodedEvent = contractInterface.parseLog(rawData);
+
+      return {
+        name,
+        type,
+        blockId,
+        address,
+        timestamp,
+        extrinsicId,
+        contractData,
+        decodedEvent,
+        abis: compiledData,
+        data: rawData.data,
+        topics: rawData.topics,
+        signedData: {fee: 0, feeDetails: 0} // TODO extract signed data from extrinsic
+      };
+    });
+
+  const transfers = await processTokenTransfers(evmLogs);
+  const tokenHolders = await processEvmTokenHolders(evmLogs);
+  // TODO Update evm events which were verified
+
+  await insertTransfers(transfers);
+  await insertTokenHolders(tokenHolders);
+}
