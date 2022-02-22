8c1def4fe52b1e59ed236947c14dd49e14b801fe
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-02-22 12:35:24 +0100

    comments added

--- crawler/src/pool/index.ts
+++ crawler/src/pool/index.ts
@@ -3,14 +3,16 @@ import config from "./../config"
 
 export default async (event: any) => {
   // TODO repare 
+  // Check if event is Reefswap factory create pool event. If so add new pool in DB
   if (event.address === config.reefswapFactoryAddress && event.method === "Create") {
     // TODO extract pool info and save it
     await insertV2('INSERT INTO pool (...)', []);
     return;
   }
 
+  // Check if current event address is in pools table 
+  // If so process pool event accordingly 
   const res = await queryv2('SELECT id FROM pool WHERE address = $1', [event.address]);
-
   if (res.length > 0) {
     switch (event.method) {
       case 'Swap': break;
