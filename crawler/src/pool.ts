94d86a2b58c64a61aec28f44b7b042a9338b64fc
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-02-28 12:13:43 +0100

    find pool event is now cleaner

--- crawler/src/pool.ts
+++ crawler/src/pool.ts
@@ -24,7 +24,11 @@ const getFirstQueryValue = async <T, >(statement: string, args = [] as any[]): P
   return res[0];
 };
 
-const findPoolEvent = async <T, >(evmEventId: string): Promise<T[]> => queryv2<T>('SELECT id FROM pool_event WHERE evm_event_id = $1;', [evmEventId]);
+interface ID {
+  id: number
+}
+
+const findPoolEvent = async (evmEventId: string): Promise<ID[]> => queryv2<ID>('SELECT id FROM pool_event WHERE evm_event_id = $1;', [evmEventId]);
 
 const getCurrentPoolPointer = async (): Promise<string> => (await getFirstQueryValue<{currval: string}>('SELECT last_value as currval FROM pool_event_sequence')).currval;
 
@@ -39,10 +43,10 @@ const findInitialIndex = async (): Promise<string> => {
   let currentEvmEventPointer = await getCurrentPoolPointer();
 
   // Initializion with current evm event pointer to make sure last pool event was written in DB
-  let doesEventExist = await findPoolEvent<{id: number}>(currentEvmEventPointer);
+  let doesEventExist = await findPoolEvent(currentEvmEventPointer);
   while (doesEventExist.length > 0) {
     currentEvmEventPointer = await getNextPoolPointer();
-    doesEventExist = await findPoolEvent<{id: number}>(currentEvmEventPointer);
+    doesEventExist = await findPoolEvent(currentEvmEventPointer);
   }
   return currentEvmEventPointer;
 };
