10e8e81e71a0ddd7ed876488486145a96f4f35f5
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-03-16 21:42:38 +0100

    verified pools query

---
+++ db/hasura/migrations/reefexplorer/1647461034975_verified_pool_migration/up.sql
@@ -0,0 +1,11 @@
+CREATE VIEW verified_pool AS
+	SELECT 
+		p.*, 
+		v1.contract_data->>'name' as name_1, 
+    v1.contract_data->>'symbol' as symbol_1,
+		v2.contract_data->>'name' as name_2,
+    v2.contract_data->>'symbol' as symbol_2
+	FROM pool AS p
+	RIGHT JOIN verified_contract AS v1 ON p.token_1 = v1.address
+	RIGHT JOIN verified_contract AS v2 ON p.token_2 = v2.address
+	WHERE p IS NOT NULL;
