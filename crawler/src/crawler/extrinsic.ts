41912dff7334d41390a9d79f56768068a302ec48
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-05-30 10:44:05 +0200

    handling transferAll events

--- crawler/src/crawler/extrinsic.ts
+++ crawler/src/crawler/extrinsic.ts
@@ -65,12 +65,32 @@ export const extrinsicBodyToTransfer = async ({
     : 'null';
 
 
+  let amount = BigNumber.from(0).toString();
   // Handling transfer all events
-  if (extrinsic.method.method === 'transfer_all') {
+  if (extrinsic.method.method === "transferAll") {
+    // To find out the value of transferred Reef amount we are calculating the account balance difference between 
+    // Blockid - 1 and bloc id
+    // This is quite hacky so if you can find a more appropriate solution implement it
+    const [hash1, hash2] = await Promise.all([
+      nodeProvider.query((provider) => provider.api.rpc.chain.getBlockHash(blockId - 1)),
+      nodeProvider.query((provider) => provider.api.rpc.chain.getBlockHash(blockId)),
+    ])
+    const [apiAt1, apiAt2] = await Promise.all([
+      nodeProvider.query((provider) => provider.api.at(hash1)),
+      nodeProvider.query((provider) => provider.api.at(hash2)),
+    ])
+    const a1 = await apiAt1.query.system.account(fromAddress);
+    const a2 = await apiAt2.query.system.account(fromAddress);
 
+    amount = BigNumber.from(a1.data.free.toString())
+      .sub(signedData!.fee.partialFee)
+      .sub(a2.data.free.toString())
+      .toString();
+  } else {
+    // Extracting transfer amount for all other transfers
+    amount = BigNumber.from(args[args.length - 1]).toString();
   }
 
-  const amount = BigNumber.from(args[args.length - 1]).toString();
   const feeAmount = BigNumber.from(signedData!.fee.partialFee).toString();
   const denom = extrinsic.method.section === 'currencies' ? args[1].token : 'REEF';
 
