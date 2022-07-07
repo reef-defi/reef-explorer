37772f1d7090567a3a9c47b96226e50fe4d0381c
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-07-07 21:27:59 +0200

    new insert evm log query

--- crawler/src/queries/evmEvent.ts
+++ crawler/src/queries/evmEvent.ts
@@ -2,9 +2,9 @@ import { utils as ethersUtils } from 'ethers/lib/ethers';
 import { GenericEventData } from '@polkadot/types/generic/Event';
 import format from 'pg-format';
 import {
-  BacktrackingEvmEvent, BytecodeLog, CompleteEvmData, Contract, DecodedEvmError, ERC20Token, EventBody, EVMEventData, VerifiedContract,
+  BacktrackingEvmEvent, BytecodeLog, CompleteEvmData, Contract, DecodedEvmError, ERC20Token, EventBody, EVMEventData, EvmLog, VerifiedContract,
 } from '../crawler/types';
-import { insert, query, queryv2 } from '../utils/connector';
+import { insert, insertV2, query, queryv2 } from '../utils/connector';
 import { toChecksumAddress } from '../utils/utils';
 
 const contractToValues = ({
@@ -127,6 +127,7 @@ const evmEventDataToInsertValue = async ({
   return `(${id}, '${parsedEvmData.raw.address}', '${JSON.stringify(parsedEvmData.raw)}', '${parsedEvmString}', '${method}', '${topics[0]}', '${topics[1]}', '${topics[2]}', '${topics[3]}', ${blockId}, ${extrinsicIndex}, ${eventIndex}, '${parsedEvmData.status}', '${parsedEvmData.type}')`;
 };
 
+// TODO deprecating
 export const insertEvmEvents = async (evmEvents: EventBody[]): Promise<void> => {
   if (evmEvents.length < 1) {
     return;
@@ -144,6 +145,42 @@ export const insertEvmEvents = async (evmEvents: EventBody[]): Promise<void> => 
       VALUES
       ${evmEventInputValues.join(',\n')};
     `);
+  }
+};
+
+interface InsertEvmLog extends CompleteEvmData {
+  eventId: number;
+  blockId: number;
+  eventIndex: number;
+  extrinsicIndex: number;
+}
+
+export const insertEvmLog = async (logs: InsertEvmLog[]): Promise<void> => {
+  if (logs.length > 0) {
+    await queryv2(
+      format(`
+        INSERT INTO evm_event
+          (event_id, contract_address, data_raw, data_parsed, method, topic_0, topic_1, topic_2, topic_3, block_id, extrinsic_index, event_index, status, type)
+        VALUES 
+          %L
+      `),
+      logs.map((log) => [
+        log.eventId,
+        log.raw.address,
+        log.raw,
+        log.parsed || null,
+        log.parsed.method || null,
+        log.raw.topics[0],
+        log.raw.topics[1],
+        log.raw.topics[2],
+        log.raw.topics[3],
+        log.blockId,
+        log.extrinsicIndex,
+        log.eventIndex,
+        log.status,
+        log.type,
+      ])
+    )
   }
 };
 
