57c048e238d231ddef264da5206637896922b6eb
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-07-19 19:03:34 +0200

    extrinsic and event id sequence migration

---
+++ db/hasura/migrations/reefexplorer/1658250037491_extrinsic&event_sequence_id_migration/down.sql
@@ -0,0 +1,6 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE SEQUENCE event_sequence START 1;
+-- CREATE SEQUENCE extrinsic_sequence START 1;
+DROP SEQUENCE event_sequence;
+DROP SEQUENCE extrinsic_sequence;
