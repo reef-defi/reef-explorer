95c11bd5cf5a4e12135e0e196a65a10fc9c28aa3
Ziga Franko G <frenkie@Zigas-MacBook-Pro.local>
2022-09-01 16:55:42 +0200

    reset pools - population script fix

--- crawler/src/populate/populate_restart_pools.ts
+++ crawler/src/populate/populate_restart_pools.ts
@@ -2,17 +2,17 @@ import { queryv2 } from '../utils/connector';
 import logger from '../utils/logger';
 
 const main = async () => {
-  logger.info('Removing candlestic data');
-  await queryv2('DELETE FROM candlestic WHERE id >= 0;');
+  logger.info('Removing candlestick data');
+  await queryv2('DELETE FROM candlestick WHERE id >= 0;');
 
   logger.info('Removing volume data');
-  await queryv2('DELETE FROM volume WHERE id >= 0;');
+  await queryv2('DELETE FROM volume_raw WHERE id >= 0;');
 
-  logger.info('Removing reserves data');
-  await queryv2('DELETE FROM reserves WHERE id >= 0;');
+  logger.info('Removing reserved data');
+  await queryv2('DELETE FROM reserved_raw WHERE id >= 0;');
 
   logger.info('Removing price data');
-  await queryv2('DELETE FROM price WHERE id >= 0;');
+  await queryv2('DELETE FROM token_price WHERE id >= 0;');
 
   logger.info('Removing pool token data');
   await queryv2('DELETE FROM pool_token WHERE id >= 0;');
@@ -24,7 +24,7 @@ const main = async () => {
   await queryv2('DELETE FROM pool WHERE id >= 0;');  
 
   logger.info('Reseting pool sequencer');
-  await queryv2('ALTER SEQUENCE pool_event_sequence RESTART WITH 0;')
+  await queryv2('ALTER SEQUENCE pool_event_sequence RESTART WITH 1;')
 };
 
 main()
