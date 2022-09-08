e95caa4c6a31f3255a102da0d6ae380196899cb7
Ziga Franko G <frenkie@Zigas-MacBook-Pro.local>
2022-09-08 20:45:07 +0200

    adding back decimals to pool

--- crawler/src/pool/events/FactoryEvent.ts
+++ crawler/src/pool/events/FactoryEvent.ts
@@ -1,8 +1,9 @@
-import { utils } from "ethers";
+import { utils, Contract } from "ethers";
+import erc20Abi from "../../assets/erc20Abi";
 import ReefswapFactory from "../../assets/ReefswapFactoryAbi";
 import config from "../../config";
 import { RawEventData } from "../../crawler/types";
-import { queryv2 } from "../../utils/connector";
+import { nodeProvider, queryv2 } from "../../utils/connector";
 import logger from "../../utils/logger";
 import TokenPrices from "../TokenPrices";
 import PoolEventBase from "./PoolEventBase";
@@ -11,6 +12,8 @@ class FactoryEvent extends PoolEventBase<RawEventData> {
   poolAddress?: string;
   tokenAddress1?: string;
   tokenAddress2?: string;
+  decimal1?: number;
+  decimal2?: number;
 
   constructor(evmEventId: string) {
     super(evmEventId);
@@ -27,29 +30,38 @@ class FactoryEvent extends PoolEventBase<RawEventData> {
 
     const [tokenAddress1, tokenAddress2, poolAddress] = data.args as string[];
 
+    const tokenContract1 = new Contract(tokenAddress1, erc20Abi, nodeProvider.getProvider())
+    const tokenContract2 = new Contract(tokenAddress2, erc20Abi, nodeProvider.getProvider())
+
+
     this.poolAddress = poolAddress;
     this.tokenAddress1 = tokenAddress1;
     this.tokenAddress2 = tokenAddress2;
+
+    this.decimal1 = await tokenContract1.decimals();
+    this.decimal2 = await tokenContract2.decimals();
 
     // Add new pool in TokenPrices
     TokenPrices.addPool(tokenAddress1, tokenAddress2);
   }
 
   async save(): Promise<void> {
-    if (!this.poolAddress || !this.tokenAddress1 || !this.tokenAddress2) {
+    if (!this.poolAddress || !this.tokenAddress1 || !this.tokenAddress2 || !this.decimal1 || !this.decimal2) {
       throw new Error("Not all required fields are set! Call process() first");
     }
 
     await queryv2(
       `INSERT INTO pool 
-        (evm_event_id, address, token_1, token_2)
+        (evm_event_id, address, token_1, token_2, decimal_1, decimal_2)
       VALUES
-        ($1, $2, $3, $4);`,
+        ($1, $2, $3, $4, $5, $6);`,
       [
         this.evmEventId, 
         this.poolAddress, 
         this.tokenAddress1, 
         this.tokenAddress2,
+        this.decimal1,
+        this.decimal2,
       ]
     );
   }
