3988bb93889614b3e2ef6580c62b4bad24fcbd4f
Ziga Franko G <frenkie@Zigas-MacBook-Pro.local>
2022-09-08 13:50:09 +0200

    update reserves, estimate price and insert price done for TokenPrices

--- crawler/src/pool/TokenPrices.ts
+++ crawler/src/pool/TokenPrices.ts
@@ -1,10 +1,14 @@
 import BigNumber from "bn.js";
-import { queryv2 } from "../utils/connector";
+import { insertV2, queryv2 } from "../utils/connector";
+import { REEF_CONTRACT_ADDRESS } from "../utils/utils";
+import { estimateTokenPriceBasedOnReefPrice } from "./pricingAlgorithm";
+import ReefPriceScrapper from "./ReefPriceScrapper";
 
 interface InitPrice {
   token_address: string;
   price: number;
 }
+
 const queryPriceData = async (blockId: string): Promise<InitPrice[]> =>
   queryv2<InitPrice>(
     "SELECT token_address, price FROM tokenPrice WHERE block_id = $1",
@@ -17,6 +21,7 @@ interface InitReserve {
   reserved_1: number;
   reserved_2: number;
 }
+
 const queryReservedData = async (blockId: string): Promise<InitReserve[]> =>
   queryv2<InitReserve>(`
     SELECT p.token_1, p.token_2, r.reserved_1, r.reserved_2 
@@ -28,9 +33,10 @@ const queryReservedData = async (blockId: string): Promise<InitReserve[]> =>
 
 
 class TokenPrices {
+  private static skip = false;
   private static tokens: string[] = [];
-  private static priceVector: BigNumber[] = [];
-  private static reserveMatrix: BigNumber[][] = [];
+  private static priceVector: number[] = [];
+  private static reserveMatrix: number[][] = [];
 
   static async init(blockId: string): Promise<void> {
     // On init reset all variables
@@ -43,45 +49,102 @@ class TokenPrices {
 
     // Add tokens to the list and price vector
     for (const {price, token_address} of priceData) {
-      this.tokens.push(token_address);
-      this.priceVector.push(new BigNumber(price));
+      this.addToken(token_address, price);
     }
-
-    // Initialize the reserve matrix
-    this.reserveMatrix = new Array(this.tokens.length).fill(
-      new Array(this.tokens.length).fill(new BigNumber(0))
-    );
 
     // Retrieve reserved data from reservedRaw table
     const reserveData = await queryReservedData(blockId);
 
     // Update the reserve matrix
     for (const {token_1, token_2, reserved_1, reserved_2} of reserveData) {
-      const i = this.tokens.indexOf(token_1);
-      const j = this.tokens.indexOf(token_2);
-      this.reserveMatrix[i][j] = new BigNumber(reserved_1);
-      this.reserveMatrix[j][i] = new BigNumber(reserved_2);
+      this.updateReserves(
+        token_1,
+        token_2,
+        reserved_1,
+        reserved_2
+      );
+    }
+
+    // If for some reason reef token is not in the list, add it
+    if (this.tokens.indexOf(REEF_CONTRACT_ADDRESS) === -1) {
+      this.addToken(REEF_CONTRACT_ADDRESS);
     }
   }
 
   static addPool(token1: string, token2: string): void {
-    // Find if any of tokens are already in the list
-    // If not add them to the list
-    // Increase the size of the reserve matrix
-    // Add prices to the price vector
+    this.addToken(token1);
+    this.addToken(token2);
   }
-  static updateReserves(token1: string, token2: string, reserve1: BigNumber, reserve2: BigNumber): void {
+
+  static updateReserves(token1: string, token2: string, reserve1: number, reserve2: number): void {
     // Find the index of the tokens in the list
+    const i = this.tokens.indexOf(token1);
+    const j = this.tokens.indexOf(token2);
+
+    // Ensure that the tokens are in the list
+    if (i === -1 || j === -1) {
+      throw new Error("Token not found");
+    }
+
     // Update the reserve matrix
+    const r1 = new BigNumber(reserve1);
+    const r2 = new BigNumber(reserve2);
+
+    this.reserveMatrix[i][j] = r1.div(r2).toNumber();
+    this.reserveMatrix[j][i] = r2.div(r1).toNumber();
+
+    this.skip = false;
   }
-  static async estimatePrice() {
+  
+  static async estimatePrice(timestamp: string): Promise<void> {
+    const reefIndex = this.tokens.indexOf(REEF_CONTRACT_ADDRESS);
+    const currentReefPrice = this.priceVector[reefIndex];
+
     // Retrieve latest reef price
+    const reefPrice = await ReefPriceScrapper.getPrice(new Date(timestamp))
+
     // Inject reef price into the price vector
-    // Solve the system of equations
-    // Update the price vector
+    this.priceVector[reefIndex] = reefPrice;
+
+    if (!this.skip || currentReefPrice !== reefPrice) {
+      // Solve the system of equations to estimate the prices
+      // Update the price vector
+      this.priceVector = estimateTokenPriceBasedOnReefPrice(
+        this.reserveMatrix,
+        this.priceVector[reefIndex],
+        reefIndex
+      );
+    }
+
+    this.skip = true;
   }
+
   static async insertPrices(blockId: string, timestamp: string): Promise<void> {
     // Insert the price vector into the database
+    await insertV2(
+      "INSERT INTO tokenPrice (block_id, timestamp, token_address, price) VALUES %L;",
+      this.priceVector.map((price, i) => [
+        blockId,
+        timestamp,
+        this.tokens[i],
+        price.toString(),
+      ])
+    );
+  }
+
+  // Private methods
+  private static addToken(token: string, price=0): void {
+    const index = this.tokens.indexOf(token);
+    if (index === -1) {
+      const oldLength = this.tokens.length;
+      // Add token to the listi
+      this.tokens.push(token);
+      // Add token to the price vector
+      this.priceVector.push(price);
+      // Add token to the reserve matrix
+      this.reserveMatrix.push(new Array(oldLength).fill(0));
+      this.reserveMatrix.forEach((row) => row.push(0));
+    }
   }
 }
 
