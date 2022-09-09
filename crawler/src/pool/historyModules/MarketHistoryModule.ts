4648680e43c88f86fd0045758d053678f25f4658
Ziga Franko G <frenkie@Zigas-MacBook-Pro.local>
2022-09-09 21:34:29 +0200

    history modules

---
+++ crawler/src/pool/historyModules/MarketHistoryModule.ts
@@ -0,0 +1,6 @@
+class MarketHistoryModule {
+  static async init(blockId: string): Promise<void> { }
+  static async save(blockId: string, timestamp: string): Promise<void> { }
+}
+
+export default MarketHistoryModule;
---
+++ crawler/src/pool/historyModules/index.ts
@@ -0,0 +1,24 @@
+import MarketHistoryModule from "./MarketHistoryModule";
+import Reserves from "./Reserves";
+import TokenPrices from "./TokenPrices";
+
+const modules = [
+  Reserves,
+  TokenPrices,
+];
+
+class MarketHistory extends MarketHistoryModule {
+  static async init(blockId: string): Promise<void> {
+    for (const module of modules) {
+      await module.init(blockId);
+    }
+  }
+
+  static async save(blockId: string, timestamp: string): Promise<void> {
+    for (const module of modules) {
+      await module.save(blockId, timestamp);
+    }
+  }
+}
+
+export default MarketHistory;
